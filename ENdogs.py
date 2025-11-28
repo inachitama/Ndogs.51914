af39e010db561cd59bd12b9266444243667d306e
Eloiise-1637 <evranckx2008@gmail.com>
2025-11-28 09:33:40 +0100

    Correct font loading based on rect.x position
    Fix conditional checks for rect.x and update font loading.

--- ENdogs.py
+++ ENdogs.py
@@ -4,6 +4,10 @@ pygame.init()
 pygame.display.set_caption("Ndogs.51914")
 fond = pygame.display.set_mode((1000, 500))
 
-if self.rect.x == 1000 - :
-
-if
+font = pygame.image.load("font1.png")
+if self.rect.x == 1000 - and font == font1.png:
+  self.rect.x = 0
+  font = pygame.image.load("font2.png")
+if self.rect.x == 1000 - and font == font2.png:
+  self.rect.x = 0
+  font = pygame.image.load("font1.png")

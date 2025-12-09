a71aaa455a28bb93c06b905ab0b5d4a000d04eab
profcandy07 <percelaycandice@gmail.com>
2025-12-09 18:42:10 +0100

    Update CNdogs.py

--- CNdogs.py
+++ CNdogs.py
@@ -11,58 +11,80 @@ class Game:
         self.all_mechant = pygame.sprite.Group()
         self.touche = {}
         self.spaw_mechant()
+        self.espace_appuye = False
 
     def spaw_mechant(self):
          mechant = Mechant(self)
          self.all_mechant.add(mechant)
 
+    def collision(self,sprite,group):
+        return pygame.sprite.spritecollide(sprite,group,False, pygame.sprite.collide_mask)
+
 
 class Joueur(pygame.sprite.Sprite):
     def __init__(self,game ):
         super().__init__()
+        self.fin_degat = 0
         self.game = game
-        self.vie = 20
-        self.max_vie = 20
-        self.poison = 1
-        self.deplacement = 5
+        self.vie = 100
+        self.max_vie = 100
+        self.attack = 10
+        self.deplacement = 4
         self.image = pygame.image.load("dog.png")  # --> mettre l'image du chien
-        self.image = pygame.transform.scale(self.image, (100, 100))
+        self.image = pygame.transform.scale(self.image, (300, 300))
         self.rect = self.image.get_rect()
         self.rect.x = 50
         self.rect.y = 300
 
+        self.last_degat_time = 0
+        self.degat_cooldown = 1000
+
+    def degat(self,montant):
+        actuel = pygame.time.get_ticks()
+        if actuel - self.fin_degat >= 1000 :
+            self.vie -= montant
+            self.fin_degat = actuel
+
+    def barre_vie(self, surface):
+            pygame.draw.rect(surface, (240, 14, 14), [self.rect.x + 50, self.rect.y -20, self.max_vie, 10])
+            pygame.draw.rect(surface, (45, 212, 21), [self.rect.x + 50, self.rect.y -20, self.vie, 10])
+
     def mouvements_droite(self):
-        self.rect.x += self.deplacement
+        if not self.game.collision(self,self.game.all_mechant):
+              self.rect.x += self.deplacement
 
     def mouvements_gauche(self):
-        self.rect.x -= self.deplacement
+            self.rect.x -= self.deplacement
 
 
 class Mechant(pygame.sprite.Sprite):
     def __init__(self,game):
         super().__init__()
         self.game = game
-        self.vie = 10
-        self.max_vie = 10
-        self.perte_vie = 1
+        self.vie = 100
+        self.max_vie = 100
+        self.attack = 5
         self.image = pygame.image.load("caca.png")  # --> mettre l'image du caca
         self.image = pygame.transform.scale(self.image, (100, 100))
         self.rect = self.image.get_rect()
-        self.rect.x = 1000
-        self.rect.y = 300
+        self.rect.x = 1050
+        self.rect.y = 500
 
-    def barre_sante(self,surface):
-        bar_color = (45, 212, 21)
-        back_color = (240, 14, 14)
+    def degats(self,montant):
+        self.vie -= montant
+        if self.vie <= 0:
+            self.game.all_mechant.remove(self)
 
-        bar_position = [self.rect.x + 10 , self.rect.y - 20,self.vie,5]
-        back_bar_position = [self.rect.x +10,self.rect.y -20,self.max_vie,5]
+    def forward(self):
+        if self.game.joueur.rect.colliderect(self.rect) :
+            now = pygame.time.get_ticks()
+        if now - self.game.joueur.last_degat_time > self.game.joueur.degat_cooldown:
+            self.game.joueur.degat(self.attack)
+            self.game.joueur.last_degat_time = now
 
-
-        pygame.draw.rect(surface,back_color,back_bar_position)
-        pygame.draw.rect(surface,bar_color,bar_position)
-
-
+    def barre_vie(self, surface):
+        pygame.draw.rect(surface, (240, 14, 14), [self.rect.x + 10, self.rect.y - 20, self.max_vie, 10])
+        pygame.draw.rect(surface, (45, 212, 21), [self.rect.x + 10, self.rect.y - 20, self.vie, 10])
 
 # fenetre jeu
 pygame.display.set_caption("Ndogs.51914")
@@ -73,23 +95,35 @@ game = Game()
 running = True
 while running:
     fond.fill((255,255,255))
-
-
+    
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             running = False
-
         if event.type == pygame.KEYDOWN:
             game.touche[event.key] = True
+            if event.key == pygame.K_SPACE:
+                game.espace_appuye = True
         if event.type == pygame.KEYUP:
             game.touche[event.key] = False
 
     fond.blit(game.joueur.image, game.joueur.rect)
     game.all_mechant.draw(fond)
-
+    game.joueur.barre_vie(fond)
 
     for mechant in game.all_mechant:
-        mechant.barre_sante(fond)
+
+        mechant.barre_vie(fond)
+
+        if game.joueur.rect.colliderect(mechant.rect):
+
+            if game.touche.get(pygame.K_SPACE):
+                mechant.vie -= 1
+                if mechant.vie <= 0:
+                    game.all_mechant.remove(mechant)
+            elif mechant.vie < mechant.max_vie:
+                mechant.vie += 1
+            else :
+                game.joueur.degat(5)
 
 
     # touche fleche

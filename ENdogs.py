052536305ef0dfed566388fc46f9e7c4d9a9f58d
Eloiise-1637 <evranckx2008@gmail.com>
2026-01-16 09:37:09 +0100

    Update ENdogs.py
    Modif fonction changement de fond, ajout d'un tutoriel

--- ENdogs.py
+++ ENdogs.py
@@ -4,7 +4,7 @@ pygame.init()
 pygame.display.set_caption("Ndogs.51914")
 fond = pygame.display.set_mode((1000, 500))
 
-#changer le fond qd collision w bord
+#changer le fond qd collision w bord V1
 font = pygame.image.load("font1.png")
 if self.rect.x == 1000 - 235 and font == font1.png:
   self.rect.x = 0
@@ -13,7 +13,7 @@ if self.rect.x == 1000 - 235 and font == font2.png:
   self.rect.x = 0
   font = pygame.image.load("font1.png")
 
-###
+#changer le fond qd collision w bord V2
 import pygame
 pygame.init()
 pygame.display.set_caption("Ndogs.51914")
@@ -21,12 +21,14 @@ fond = pygame.display.set_mode((1000, 500))
 
 font = pygame.image.load("font1.png")
 def background():    
-    if self.rect.x == 1000 - :  # mettre moitié taille x chien
-        self.rect.x = 0
+    if game.joueur.rect.x == 1000 - 235 :
+        game.joueur.rect.x = 0
         if font == font1.png:
             font = pygame.image.load("font1.png")
         else:
             pygame.image.load("font2.png")
+
+#afficher du texte
 text_x = 100
 text_y = 720
 col = (250, 250, 250)
@@ -35,10 +37,10 @@ def draw_text(text, font, col, text_x, text_y):
     screen.blit(img, (text_x, text_y))
 
 def collision():                                  # -> ligne 118 code Candice
-    if joueur.image.colliderect(mechant.image):
-        drax_text("Cliquez sur la touche 'Espace' pour vaincre le caca.", font, col, text_x, text_y)
+  if game.joueur.image.colliderect(mechant.image):
+    drax_text("Cliquez sur la touche 'Espace' pour vaincre le caca.", font, col, text_x, text_y)
         
-#quand jeu commence:
+#quand jeu commence: (tutoriel)
   while not game.touche.get(pygame.K_LEFT):
     draw text("Cliquez sur la flèche gauche pour avancer vers la gauche.", font, col, text_x, text_y)
   while not game.touche.get(pygame.K_RIGHT):

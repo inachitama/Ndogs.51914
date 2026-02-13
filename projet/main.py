377557ab0f26b6938bfdc676718759031b8a2241
profcandy07 <percelaycandice@gmail.com>
2026-02-13 09:01:48 +0100

    Update main.py

--- projet/main.py
+++ projet/main.py
@@ -63,6 +63,7 @@ class Joueur(pygame.sprite.Sprite):
               self.image = self.image_d
 
     def mouvements_gauche(self):
+        if not self.game.collision(self,self.game.all_mechant):
             self.rect.x -= self.deplacement
             self.image = self.image_g
 
@@ -194,3 +195,4 @@ def lancer_jeu():
 
 
     pygame.quit()
+
